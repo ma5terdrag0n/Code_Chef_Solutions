a53e514937d7a9b6b5f62160458dbd870ff435a4
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-06 17:48:49 +0530

    Create HOLES.cpp

---
+++ HOLES.cpp
@@ -0,0 +1,27 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com/category/hackerrank-solutions/
+*
+*/
+#include<iostream>
+using namespace std ;
+void op(string s);
+int main(){ 
+  int  t,i;
+  cin>>t;
+  string s[t];
+  for(i=0;i<t;i++)
+  cin>>s[i];
+  for(i = 0;i<t;i++)
+  op(s[i]);
+}
+void op(string s){
+  int n ,a=0,i;
+  n=s.size();
+  for(i=0;i<n;i++){
+     if (s[i]=='A'||s[i]=='D'||s[i]=='O'|s[i]=='R'||s[i]=='P' ||s[i]=='Q') a++;
+     if(s[i]=='B') a=a+2;
+   }
+cout<<a<<endl;
+}
