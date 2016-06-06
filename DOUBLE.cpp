64540f2342b959456e7d4926dfe9805bf332b6b5
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-06 17:49:54 +0530

    Create DOUBLE.cpp

---
+++ DOUBLE.cpp
@@ -0,0 +1,24 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com/category/hackerrank-solutions/
+*
+*/
+#include<iostream>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   long long int n[1000000],i;
+   for(i=0;i<t;i++){
+   cin>>n[i];
+   }
+   for(i=0;i<t;i++){
+   if(n[i]%2==0)
+      cout<<n[i]<<endl;
+   else
+      cout<<n[i]-1<<endl;
+   }
+   return 0;
+}
+ 
