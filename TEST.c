1a509aa738ba0d23829dec867b7b75c8ff54d679
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-06 17:44:28 +0530

    Create TEST.c

---
+++ TEST.c
@@ -0,0 +1,18 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com/category/hackerrank-solutions/
+*
+*/
+  #include<stdio.h>
+  int main()
+  {
+  int n,i;
+  for(i=0;i<100;i++)
+  {
+  scanf("%d",&n);
+  if(n==42)
+  break;
+  printf("%d\n",n);
+  }
+  }
