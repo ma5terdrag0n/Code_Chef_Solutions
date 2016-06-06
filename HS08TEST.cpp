5b9117a9eb12fd74d848a278919f4bae4e745036
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-06 17:45:53 +0530

    Create HS08TEST.cpp

---
+++ HS08TEST.cpp
@@ -0,0 +1,24 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com/category/hackerrank-solutions/
+*
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+float b;
+cin>>a;
+cin>>b;
+if(a%5!=0||(float)a>b-5)
+{
+cout<<endl<<b;
+}
+else
+{
+cout<<endl<<float(b-0.5-a);
+}
+return 0;
+}   
