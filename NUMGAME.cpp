49fc82ec6cfb4b9b546a8f88945853058fa73f38
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-06 17:50:58 +0530

    Create NUMGAME.cpp

---
+++ NUMGAME.cpp
@@ -0,0 +1,25 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com/category/hackerrank-solutions/
+*
+*/
+#include<iostream>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   long long int n[1000000],i,a;
+   for(i=0;i<t;i++){
+   cin>>n[i];
+   }
+   for(i=0;i<t;i++)
+   {
+   a=n[i];
+ 
+   if(a%2!=0) cout<<"BOB"<<endl;
+   if(a%2==0) cout<<"ALICE"<<endl;
+ 
+   }
+   return 0;
+}
