d4b264e87fdc62039c794287d135eaa8895cfe47
Pritish Thakkar <prince.thakkar001@gmail.com>
2016-06-15 12:42:07 +0530

    Create FSQRT.c

---
+++ FSQRT.c
@@ -0,0 +1,20 @@
+/*
+*
+* Solution By : Pritish Thakkar
+* Reference : https://gocodergo.wordpress.com
+*
+*/
+#include<stdio.h>
+#include<math.h>
+ 
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int a;
+		scanf("%d",&a);
+		int s=sqrt(a);
+		printf("%d\n",s);
+	}
+	return 0;
+} 
